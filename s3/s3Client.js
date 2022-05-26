146ac2c27e8f3007fd93591a38f9797282251915
dannyboi07 <danielchettiar07@gmail.com>
2022-05-26 20:21:10 +0530

    Moved S3 details to .env

--- s3/s3Client.js
+++ s3/s3Client.js
@@ -1,56 +1,62 @@
-const { S3Client, ListBucketsCommand, GetObjectCommand, PutObjectCommand } = require("@aws-sdk/client-s3");
+const {
+	S3Client,
+	ListBucketsCommand,
+	GetObjectCommand,
+	PutObjectCommand,
+} = require("@aws-sdk/client-s3");
 const fs = require("fs");
 
-const region = "ap-south-1";
-const credentials = { 
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID, 
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY 
+const region = process.env.AWS_S3_REGION;
+const credentials = {
+	accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 };
+const Bucket = process.env.AWS_S3_BUCKET_NAME;
 
-// Reminder: Currently using keys of IAM user that was created specifically 
+// Reminder: Currently using keys of IAM user that was created specifically
 // just for accessing the bucket
 
 const s3Client = new S3Client({ credentials, region });
 
 function uploadProfImg(filePath) {
-    const fileReadStream = fs.createReadStream(filePath);
+	const fileReadStream = fs.createReadStream(filePath);
 
-    const uploadParams = {
-        Bucket: "socio-us-east-1",
-        Key: `public/profile-pics/${file.filename}`,
-        Body: fileReadStream
-    };
+	const uploadParams = {
+		Bucket,
+		Key: `public/profile-pics/${file.filename}`,
+		Body: fileReadStream,
+	};
 
-    return s3Client.send(new PutObjectCommand(uploadParams));
+	return s3Client.send(new PutObjectCommand(uploadParams));
 }
 
 function uploadFile(filePath) {
-    const fileReadStream = fs.createReadStream(filePath);
+	const fileReadStream = fs.createReadStream(filePath);
 
-    const uploadParams = {
-        Bucket: "socio-us-east-1",
-        Key: `public/post-images/${file.filename}`,
-        Body: fileReadStream
-    };
+	const uploadParams = {
+		Bucket,
+		Key: `public/post-images/${file.filename}`,
+		Body: fileReadStream,
+	};
 
-    return s3Client.send(new PutObjectCommand(uploadParams));
-};
-
-function getS3Obj(Key) {
-    console.log(Key);
-    const getParams = {
-        Bucket: "socio-database-dev",
-        Key
-    };
-
-    return s3Client.send(new GetObjectCommand(getParams))
+	return s3Client.send(new PutObjectCommand(uploadParams));
 }
 
-module.exports = { 
-    s3Client,
-    ListBucketsCommand,
-    GetObjectCommand,
-    uploadFile,
-    uploadProfImg,
-    getS3Obj
- };
+function getS3Obj(Key) {
+	console.log(Key);
+	const getParams = {
+		Bucket,
+		Key,
+	};
+
+	return s3Client.send(new GetObjectCommand(getParams));
+}
+
+module.exports = {
+	s3Client,
+	ListBucketsCommand,
+	GetObjectCommand,
+	uploadFile,
+	uploadProfImg,
+	getS3Obj,
+};
